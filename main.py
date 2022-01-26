3d59ade046aa469a9db5a45a29c284eb6794780b
LHERMITE Thomas <lhertho@ndlp.fr>
2022-01-26 15:42:11 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,9 @@
+x=(int(input("Choississez la coordonnée x")))
+y=(int(input("Choississez la coordonnée y")))
+if x == 4 and y == 4 :
+    print( "Touché" )
+elif x == 4 or y == 4 :
+    print( "en vue" )
+else:
+    print("A l'eau")
+
